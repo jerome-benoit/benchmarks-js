31688ebd341c01ea9029dcf57f4c77fe92aa455c
Jérôme Benoit <jerome.benoit@sap.com>
2022-10-23 10:17:31 +0200

    Refine lint-staged configuration
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  '*.{js,jsx}': ['prettier --write', 'standard --fix', 'eslint --cache --fix'],
+  '*.{js,jsx,mjs,cjs}': [
+    'prettier --write',
+    'standard --fix',
+    'eslint --cache --fix'
+  ],
   '*.{json,md,yml,yaml}': ['prettier --write']
 }
