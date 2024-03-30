26792d4d3d5f5666dd72e7778e3baa8d370d9b03
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-03-30 21:51:29 +0100

    feat: add JSON.stringify benchmark
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ json-stringify.mjs
@@ -0,0 +1,22 @@
+import { bench, group, run } from 'mitata'
+
+const sampleObj = {
+  name: 'Sid',
+  age: 29,
+  engineer: true,
+  expertise: ['html', 'css', 'react'],
+  address: {
+    city: 'New york',
+    state: 'NY'
+  }
+}
+
+group('JSON stringify', () => {
+  bench('JSON.stringify', () => {
+    JSON.stringify(sampleObj)
+  })
+})
+
+await run({
+  units: true
+})
