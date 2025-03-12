f38e8dfb293298afabcba133fb070a1a4682c4da
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-03-12 14:05:22 +0100

    chore: switch to eslint defineConfig
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 /* eslint-disable n/no-unpublished-import */
 import js from '@eslint/js'
-import { defineFlatConfig } from 'eslint-define-config'
 import jsdoc from 'eslint-plugin-jsdoc'
 import perfectionist from 'eslint-plugin-perfectionist'
+import { defineConfig } from 'eslint/config'
 import neostandard, { plugins } from 'neostandard'
 
-export default defineFlatConfig([
+export default defineConfig([
   js.configs.recommended,
   plugins.promise.configs['flat/recommended'],
   plugins.n.configs['flat/recommended'],
